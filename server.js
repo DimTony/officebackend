6b13739adf0c0a3a31393155e18186a00127db97
Obinna Anthony Dimojiaku <dimojiakuo@accessbankplc.com>
2024-12-20 14:58:02 +0100

    cors fix 6

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const { Server } = require("socket.io");
 
+const USER_URL = process.env.USER_URL;
+const ADMIN_URL = process.env.ADMIN_URL;
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "https://montreux-hoa.vercel.app",
-      "https://officeadmin-ochre.vercel.app",
-    ],
+    origin: "*", // List your allowed URLs
     methods: ["GET", "POST"],
   },
 });
